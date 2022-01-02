c97c90684520f1b254b73ba93dbe948faa085a9a
Javier Vargas <javiervargasvico@hotmail.com>
2022-01-02 21:44:45 +0100

    Updated path

--- task_ss.sh
+++ task_ss.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export DISPLAY=:0
-source /Users/Javi/Downloads/SS_notifier/myenv/bin/activate
-python /Users/Javi/Downloads/SS_notifier/ss_notifier.py
+source /Users/Javi/Downloads/seguridad_social_disponibilidad_bot/myenv/bin/activate
+python /Users/Javi/Downloads/seguridad_social_disponibilidad_bot/ss_notifier.py
